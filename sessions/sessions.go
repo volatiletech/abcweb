2309c83f3c11775b02b0b5c5e59cf51842ea3a82
Patrick O'brien <nullbio@users.noreply.github.com>
2016-11-29 01:41:05 +1000

    Add impl for json helpers

--- sessions/sessions.go
+++ sessions/sessions.go
@@ -1,6 +1,9 @@
 package sessions
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 // Storer provides methods to retrieve, add and delete session keys
 // and their corresponding values.
@@ -34,26 +37,57 @@ func IsNoSessionError(err error) bool {
 	return ok
 }
 
+// Put is a JSON helper used for storing key-value session values.
+// Put stores in the session a marshaled version of the passed in map[string]string.
+func Put(overseer Overseer, w http.ResponseWriter, r *http.Request, v map[string]string) (*http.Request, error) {
+	ret, err := json.Marshal(v)
+	if err != nil {
+		return nil, err
+	}
+
+	return overseer.Put(w, r, string(ret))
+}
+
 // Get is a JSON helper used for retrieving key-value session values.
 // Get returns the unmarshaled session value as a map[string]string.
 func Get(overseer Overseer, w http.ResponseWriter, r *http.Request) (map[string]string, error) {
+	var ret map[string]string
+	val, err := overseer.Get(w, r)
+	if err != nil {
+		return nil, err
+	}
 
-}
+	err = json.Unmarshal([]byte(val), &ret)
+	if err != nil {
+		return nil, err
+	}
 
-// Put is a JSON helper used for storing key-value session values.
-// Put stores in the session a marshaled version of the passed in map[string]string.
-func Put(overseer Overseer, w http.ResponseWriter, r *http.Request, value map[string]string) (*http.Request, error) {
-
-}
-
-// GetObj is a JSON helper used for retrieving object or variable session values.
-// GetObj unmarshals the session value into the value pointed to by v.
-func GetObj(overseer Overseer, w http.ResponseWriter, r *http.Request, v interface{}) error {
-
+	return ret, nil
 }
 
 // PutObj is a JSON helper used for storing object or variable session values.
 // Put stores in the session a marshaled version of the passed in value pointed to by v.
 func PutObj(overseer Overseer, w http.ResponseWriter, r *http.Request, v interface{}) (*http.Request, error) {
+	ret, err := json.Marshal(v)
+	if err != nil {
+		return nil, err
+	}
 
+	return overseer.Put(w, r, string(ret))
+}
+
+// GetObj is a JSON helper used for retrieving object or variable session values.
+// GetObj unmarshals the session value into the value pointed to by v.
+func GetObj(overseer Overseer, w http.ResponseWriter, r *http.Request, v interface{}) error {
+	val, err := overseer.Get(w, r)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal([]byte(val), v)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- sessions/sessions_test.go
+++ sessions/sessions_test.go
@@ -1,1 +1,23 @@
 package sessions
+
+import "testing"
+
+func TestPut(t *testing.T) {
+	t.Parallel()
+
+}
+
+func TestGet(t *testing.T) {
+	t.Parallel()
+
+}
+
+func TestPutObj(t *testing.T) {
+	t.Parallel()
+
+}
+
+func TestGetObj(t *testing.T) {
+	t.Parallel()
+
+}
