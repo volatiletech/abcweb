f6a357ff59a77ce6b1d99cabf85717eb3d998927
Patrick O'brien <nullbio@users.noreply.github.com>
2016-11-29 01:57:09 +1000

    Finished tests for json session helpers

--- sessions/sessions_test.go
+++ sessions/sessions_test.go
@@ -1,23 +1,68 @@
 package sessions
 
-import "testing"
+import (
+	"net/http/httptest"
+	"reflect"
+	"testing"
+)
 
-func TestPut(t *testing.T) {
+func TestPutAndGet(t *testing.T) {
 	t.Parallel()
 
+	r := httptest.NewRequest("GET", "http://localhost", nil)
+	w := httptest.NewRecorder()
+
+	m, _ := NewDefaultMemoryStorer()
+	s := NewStorageOverseer(NewCookieOptions(), m)
+
+	val := map[string]string{
+		"hi": "hello",
+	}
+
+	r, err := Put(s, w, r, val)
+	if err != nil {
+		t.Error(err)
+	}
+
+	ret, err := Get(s, w, r)
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(val, ret) {
+		t.Errorf("Expected ret to match val, got: %#v", ret)
+	}
 }
 
-func TestGet(t *testing.T) {
-	t.Parallel()
-
+type TestSessJSON struct {
+	Test string
 }
 
-func TestPutObj(t *testing.T) {
+func TestPutAndGetObj(t *testing.T) {
 	t.Parallel()
 
-}
+	r := httptest.NewRequest("GET", "http://localhost", nil)
+	w := httptest.NewRecorder()
 
-func TestGetObj(t *testing.T) {
-	t.Parallel()
+	m, _ := NewDefaultMemoryStorer()
+	s := NewStorageOverseer(NewCookieOptions(), m)
+
+	val := &TestSessJSON{
+		Test: "hello",
+	}
+
+	r, err := PutObj(s, w, r, val)
+	if err != nil {
+		t.Error(err)
+	}
+
+	testptr := &TestSessJSON{}
+
+	err = GetObj(s, w, r, testptr)
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(val, testptr) {
+		t.Errorf("Expected testptr to match val, got: %#v", testptr)
+	}
 
 }
