c41a92bf652e518d7b9475b9205cf3ae0adeb909
Patrick O'brien <nullbio@users.noreply.github.com>
2017-01-01 12:16:26 +1000

    Add rand string func

--- cmd/template_funcs.go
+++ cmd/template_funcs.go
@@ -1,10 +1,27 @@
 package cmd
 
 import (
+	"math/rand"
 	"strings"
 	"text/template"
+	"time"
 )
 
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
 var templateFuncs = template.FuncMap{
-	"titleCase": func(s string) string { return strings.ToUpper(s) },
+	"titleCase":  func(s string) string { return strings.ToUpper(s) },
+	"randString": randString,
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+func randString(length int) string {
+	b := make([]rune, length)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
 }
