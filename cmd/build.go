8689bb25de5f86a097ca204d5412c30380d1a9eb
Patrick O'brien <nullbio@users.noreply.github.com>
2017-04-11 12:22:02 +1000

    finish build command

--- cmd/build.go
+++ cmd/build.go
@@ -1,8 +1,12 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var buildCmdConfig buildConfig
@@ -10,34 +14,86 @@ var buildCmdConfig buildConfig
 // buildCmd represents the new command
 var buildCmd = &cobra.Command{
 	Use:     "build",
-	Short:   "Builds your ABCWeb app binary and.... does stuff to assets?",
-	Long:    "describe build command here",
-	Example: "build example here",
-	PreRunE: buildCmdPreRun,
+	Short:   "Builds your ABCWeb binary and executes the gulp build task",
+	Long:    "Builds your ABCWeb binary and executes the gulp build task",
+	Example: "abcweb build",
 	RunE:    buildCmdRun,
 }
 
 func init() {
-	buildCmd.Flags().StringP("sessions-prod-storer", "p", "disk", "Session storer to use in production mode")
-	buildCmd.Flags().BoolP("silent", "", false, "Disable console output")
+	buildCmd.Flags().BoolP("go-only", "g", false, "Only watch and rebuild the go app by piping to refresh app")
+	buildCmd.Flags().BoolP("assets-only", "a", false, "Only watch and build the assets by piping to gulp watch")
 
 	RootCmd.AddCommand(buildCmd)
 }
 
-func buildCmdPreRun(cmd *cobra.Command, args []string) error {
-	var err error
+func buildCmdRun(cmd *cobra.Command, args []string) error {
+	cnf.ModeViper.BindPFlags(cmd.Flags())
 
-	viper.BindPFlags(cmd.Flags())
+	if !cnf.ModeViper.GetBool("go-only") {
+		fmt.Println("Building assets...")
+		if err := buildAssets(); err != nil {
+			return err
+		}
+	}
 
-	//	buildCmdConfig = newConfig{
-	//		NoGitIgnore: viper.GetBool("no-gitignore"),
-	//		DefaultEnv:  viper.GetString("env"),
-	//		Bootstrap:   strings.ToLower(viper.GetString("bootstrap")),
-	//	}
+	if !cnf.ModeViper.GetBool("assets-only") {
+		fmt.Println("Building Go app...")
+		if err := buildApp(); err != nil {
+			return err
+		}
+	}
 
-	return err
+	return nil
 }
 
-func buildCmdRun(cmd *cobra.Command, args []string) error {
+func buildApp() error {
+	_, err := exec.LookPath("go")
+	if err != nil {
+		fmt.Println("Cannot Go in PATH.")
+		return nil
+	}
+
+	cmd := exec.Command("go", "build")
+	cmd.Dir = cnf.AppPath
+
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	if err = cmd.Run(); err != nil {
+		return err
+	}
+
+	fmt.Printf("Build app complete.\n\n")
+	return nil
+}
+
+func buildAssets() error {
+	_, err := os.Stat(filepath.Join(cnf.AppPath, "gulpfile.js"))
+	if os.IsNotExist(err) {
+		fmt.Println("No gulpfile.js present, skipping gulp build")
+		return nil
+	} else if err != nil {
+		return err
+	}
+
+	_, err = exec.LookPath("gulp")
+	if err != nil {
+		fmt.Println("Cannot find gulp in PATH. Is it installed?")
+		return nil
+	}
+
+	cmd := exec.Command("gulp", "build")
+
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	if err = cmd.Run(); err != nil {
+		return err
+	}
+
+	fmt.Printf("Build assets complete.\n\n")
 	return nil
 }
