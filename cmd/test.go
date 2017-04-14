d3ccd3be14c8e7c9ad214ce009ff905f6c63edfd
Patrick O'brien <nullbio@users.noreply.github.com>
2017-04-14 11:47:01 +1000

    make test command ignore vendor
    * fix race for parallel tests in InitRenderer

--- cmd/test.go
+++ cmd/test.go
@@ -4,15 +4,17 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"regexp"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
 var testCmd = &cobra.Command{
-	Use:   "test",
-	Short: "Runs the tests for your abcweb app",
-	Long: `Runs all Go tests for your abcweb app by executing "go test ./..."
-from the root directory of your app.`,
+	Use:     "test",
+	Short:   "Runs the tests for your abcweb app",
+	Long:    `Runs all Go tests for your abcweb app and skips your vendor directory.`,
 	Example: "abcweb test -v",
 	Run:     testCmdRun,
 }
@@ -28,21 +30,39 @@ func init() {
 }
 
 func testCmdRun(cmd *cobra.Command, args []string) {
+	checkDep("go")
+
 	var exc *exec.Cmd
 
-	// Set the APPNAME_ENV flag to the env value in case it was obtained
-	// through the config file instead of through an environment variable.
-	// This is required so that the models tests know what environment to load.
-	// os.Setenv(strmangle.EnvAppName(cnf.GetAppName(cnf.AppPath))+"_ENV", cnf.ActiveEnv)
+	exc = exec.Command("go", "list", "./...")
+	exc.Dir = cnf.AppPath
+	out, err := exc.CombinedOutput()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
 	if cnf.ModeViper.GetBool("verbose") {
-		exc = exec.Command("go", "test", "./...", "-v")
+		exc = exec.Command("go", "test", "-v", "-p", "1")
 	} else {
-		exc = exec.Command("go", "test", "./...")
+		exc = exec.Command("go", "test", "-p", "1")
+	}
+
+	rgx, err := regexp.Compile(filepath.Join(cnf.AppName, "vendor"))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	pkgs := strings.Split(strings.TrimSpace(string(out)), "\n")
+	for _, pkg := range pkgs {
+		if !rgx.MatchString(pkg) {
+			exc.Args = append(exc.Args, pkg)
+		}
 	}
 
 	exc.Dir = cnf.AppPath
-	out, err := exc.CombinedOutput()
+	out, err = exc.CombinedOutput()
 	fmt.Print(string(out))
 
 	if err != nil {
