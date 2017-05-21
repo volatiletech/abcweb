e17529b2c42f002d2b1838dcaf53f43d4a986c47
Patrick O'brien <nullbio@users.noreply.github.com>
2017-05-21 22:57:39 +1000

    add recursive nightmare function

--- abcconfig/config.go
+++ abcconfig/config.go
@@ -12,6 +12,7 @@ import (
 	"github.com/spf13/viper"
 )
 
+// The config filename, overwritten in tests to point to a tmp file
 var filename = "config.toml"
 
 // AppConfig struct includes the necessary abcweb config components.
@@ -19,6 +20,9 @@ var filename = "config.toml"
 // to your configuration you can do so, but make sure you include
 // *ServerConfig objects and *DBConfig objects like below (if desired).
 type AppConfig struct {
+	// The active environment section
+	Env string `toml:"env" mapstructure:"env" env:"ENV"`
+
 	Server *ServerConfig `toml:"server" mapstructure:"server"`
 	DB     *DBConfig     `toml:"db" mapstructure:"db"`
 }
@@ -26,63 +30,70 @@ type AppConfig struct {
 // ServerConfig is config for the app loaded through environment variables,
 // command line, or the config.toml file.
 type ServerConfig struct {
-	// The active config file environment mode.
-	// This sits at the root level of the config file (above the envs).
-	ActiveEnv string `toml:"env" mapstructure:"env"`
 	// LiveReload enabled or disabled
-	LiveReload bool `toml:"live-reload" mapstructure:"live-reload"`
+	LiveReload bool `toml:"live-reload" mapstructure:"live-reload" env:"SERVER_LIVE_RELOAD"`
 	// Use the production logger (JSON and log level warn) or the
 	// development logger (console and log level info)
-	ProdLogger bool `toml:"prod-logger" mapstructure:"prod-logger"`
+	ProdLogger bool `toml:"prod-logger" mapstructure:"prod-logger" env:"SERVER_PROD_LOGGER"`
 	// http bind address. ":<port>" for all interfaces
-	Bind string `toml:"bind" mapstructure:"bind"`
+	Bind string `toml:"bind" mapstructure:"bind" env:"SERVER_BIND"`
 	// https bind address. ":<port>" for all interfaces
-	TLSBind string `toml:"tls-bind" mapstructure:"tls-bind"`
+	TLSBind string `toml:"tls-bind" mapstructure:"tls-bind" env:"SERVER_TLS_BIND"`
 	// TLS certificate file path
-	TLSCertFile string `toml:"tls-cert-file" mapstructure:"tls-cert-file"`
+	TLSCertFile string `toml:"tls-cert-file" mapstructure:"tls-cert-file" env:"SERVER_TLS_CERT_FILE"`
 	// TLS key file path
-	TLSKeyFile string `toml:"tls-key-file" mapstructure:"tls-key-file"`
+	TLSKeyFile string `toml:"tls-key-file" mapstructure:"tls-key-file" env:"SERVER_TLS_KEY_FILE"`
 	// Maximum duration before timing out read of the request
-	ReadTimeout time.Duration `toml:"read-timeout" mapstructure:"read-timeout"`
+	ReadTimeout time.Duration `toml:"read-timeout" mapstructure:"read-timeout" env:"SERVER_READ_TIMEOUT"`
 	// Maximum duration before timing out write of the response
-	WriteTimeout time.Duration `toml:"write-timeout" mapstructure:"write-timeout"`
+	WriteTimeout time.Duration `toml:"write-timeout" mapstructure:"write-timeout" env:"SERVER_WRITE_TIMEOUT"`
 	// Maximum duration before timing out idle keep-alive connection
-	IdleTimeout time.Duration `toml:"idle-timeout" mapstructure:"idle-timeout"`
+	IdleTimeout time.Duration `toml:"idle-timeout" mapstructure:"idle-timeout" env:"SERVER_IDLE_TIMEOUT"`
 	// Use manifest.json assets mapping
-	AssetsManifest bool `toml:"assets-manifest" mapstructure:"assets-manifest"`
+	AssetsManifest bool `toml:"assets-manifest" mapstructure:"assets-manifest" env:"SERVER_ASSETS_MANIFEST"`
 	// Disable browsers caching asset files by setting response headers
-	AssetsNoCache bool `toml:"assets-no-cache" mapstructure:"assets-no-cache"`
+	AssetsNoCache bool `toml:"assets-no-cache" mapstructure:"assets-no-cache" env:"SERVER_ASSETS_NO_CACHE"`
 	// RenderRecompile enables recompilation of the template on every render call.
 	// This should be used in development mode so no server restart is required
 	// on template file changes.
-	RenderRecompile bool `toml:"render-recompile" mapstructure:"render-recompile"`
+	RenderRecompile bool `toml:"render-recompile" mapstructure:"render-recompile" env:"SERVER_RENDER_RECOMPILE"`
 	// Use the development mode sessions storer opposed to production mode storer
 	// defined in app/sessions.go -- Usually a cookie storer for dev
 	// and disk storer for prod.
-	SessionsDevStorer bool `toml:"sessions-dev-storer" mapstructure:"sessions-dev-storer"`
+	SessionsDevStorer bool `toml:"sessions-dev-storer" mapstructure:"sessions-dev-storer" env:"SERVER_SESSIONS_DEV_STORER"`
 	// PublicPath defaults to "public" but can be set to something else
 	// by the {{.AppEnvName}}_PUBLIC_PATH environment variable.
 	// This is set by the "abcweb dev" command to instruct the app to
 	// load assets from a /tmp folder instead of the local public folder.
-	PublicPath string `toml:"public-path" mapstructure:"public-path"`
+	PublicPath string `toml:"public-path" mapstructure:"public-path" env:"SERVER_PUBLIC_PATH"`
 }
 
 // DBConfig holds the database config for the app loaded through
 // environment variables, or the config.toml file.
 type DBConfig struct {
 	// DB is the database software; "postgres", "mysql", etc.
-	DB string `toml:"db" mapstructure:"db"`
+	DB string `toml:"db" mapstructure:"db" env:"DB_DB"`
 	// The database name
-	DBName  string `toml:"dbname" mapstructure:"dbname"`
-	Host    string `toml:"host" mapstructure:"host"`
-	Port    int    `toml:"port" mapstructure:"port"`
-	User    string `toml:"user" mapstructure:"user"`
-	Pass    string `toml:"pass" mapstructure:"pass"`
-	SSLMode string `toml:"sslmode" mapstructure:"sslmode"`
+	DBName  string `toml:"dbname" mapstructure:"dbname" env:"DB_DBNAME"`
+	Host    string `toml:"host" mapstructure:"host" env:"DB_HOST"`
+	Port    int    `toml:"port" mapstructure:"port" env:"DB_PORT"`
+	User    string `toml:"user" mapstructure:"user" env:"DB_USER"`
+	Pass    string `toml:"pass" mapstructure:"pass" env:"DB_PASS"`
+	SSLMode string `toml:"sslmode" mapstructure:"sslmode" env:"DB_SSLMODE"`
 
 	// Throw an error when the app starts if the database is not
 	// using the latest migration
-	EnforceMigration bool `toml:"enforce-migration" mapstructure:"enforce-migration"`
+	EnforceMigration bool `toml:"enforce-migration" mapstructure:"enforce-migration" env:"DB_ENFORCE_MIGRATION"`
+}
+
+// envAppName is the app name uppercased to prefix environment variables
+// i.e. "my app" translates to "MY_APP". It gets the app name using a git
+// command on the project dir.
+var envAppName string
+
+// SetEnvAppName sets the envAppName variable
+func SetEnvAppName(name string) {
+	envAppName = name
 }
 
 // NewAppConfig returns an initialized AppConfig object
@@ -130,37 +141,124 @@ func InitAppConfig(flags *pflag.FlagSet, cfg interface{}) error {
 	return nil
 }
 
-// NewFlagSet creates the set of flags specific to the server config and
-// other relevant config (like enforce-migration etc).
+type mapping struct {
+	chain string
+	env   string
+}
+
+func getTagMappingsRecursive(chain string, v reflect.Value) ([]mapping, error) {
+	mappings := []mapping{}
+
+	for i := 0; i < v.NumField(); i++ {
+		cv := v.Field(i)
+		tag := v.Type().Field(i).Tag
+
+		ms := tag.Get("mapstructure")
+		env := tag.Get("env")
+
+		if cv.Kind() == reflect.Ptr {
+			nv := reflect.Indirect(cv)
+			if !nv.IsValid() {
+				return nil, fmt.Errorf("cannot access non-initialized pointer %#v", cv)
+			}
+			// Only indirect struct types, if they're valid
+			if nv.Kind() == reflect.Struct {
+				cv = nv
+			}
+		}
+
+		// nc = newchain
+		var nc string
+		if chain != "" {
+			nc = strings.Join([]string{chain, ms}, ".")
+		} else {
+			nc = ms
+		}
+
+		switch cv.Kind() {
+		case reflect.Struct:
+			m, err := getTagMappingsRecursive(nc, cv)
+			if err != nil {
+				return nil, err
+			}
+			mappings = append(mappings, m...)
+		default:
+			if env != "" && ms != "" {
+				mappings = append(mappings, mapping{chain: nc, env: env})
+			}
+		}
+	}
+
+	return mappings, nil
+}
+
+func GetTagMappings(cfg interface{}) ([]mapping, error) {
+	return getTagMappingsRecursive("", reflect.Indirect(reflect.ValueOf(cfg)))
+}
+
+// NewFlagSet creates the set of flags specific to the server and db config
+// and the root level config (like --version, --env)
 func NewFlagSet() *pflag.FlagSet {
 	flags := &pflag.FlagSet{}
 
-	flags.BoolP("live-reload", "", false, "Enable or disable LiveReload")
-	flags.BoolP("prod-logger", "", true, "Use the production logger, JSON and log level warn")
-	flags.StringP("bind", "", ":80", `HTTP bind address, eg: ":80"`)
-	flags.StringP("tls-bind", "", "", `HTTPS bind address, eg: ":443"`)
-	flags.StringP("tls-cert-file", "", "", "TLS certificate file path")
-	flags.StringP("tls-key-file", "", "", "TLS key file path")
-	flags.DurationP("read-timeout", "", time.Second*10, "Maximum duration before timing out read of the request")
-	flags.DurationP("write-timeout", "", time.Second*15, "Maximum duration before timing out write of the response")
-	flags.DurationP("idle-timeout", "", time.Second*120, "Maximum duration before timing out idle keep-alive connection")
+	flags.AddFlagSet(NewRootFlagSet())
+	flags.AddFlagSet(NewServerFlagSet())
+	flags.AddFlagSet(NewDBFlagSet())
 
+	return flags
+}
+
+func NewRootFlagSet() *pflag.FlagSet {
+	flags := &pflag.FlagSet{}
+
+	// root level flags
+	flags.BoolP("version", "", false, "Display the build version hash")
+	flags.StringP("env", "e", "prod", "The config files environment to load")
+
+	return flags
+}
+
+func NewServerFlagSet() *pflag.FlagSet {
+	flags := &pflag.FlagSet{}
+
+	// server subsection flags
+	flags.BoolP("server.live-reload", "", false, "Enable or disable LiveReload")
+	flags.BoolP("server.prod-logger", "", true, "Use the production logger, JSON and log level warn")
+	flags.StringP("server.bind", "", ":80", `HTTP bind address, eg: ":80"`)
+	flags.StringP("server.tls-bind", "", "", `HTTPS bind address, eg: ":443"`)
+	flags.StringP("server.tls-cert-file", "", "", "TLS certificate file path")
+	flags.StringP("server.tls-key-file", "", "", "TLS key file path")
+	flags.DurationP("server.read-timeout", "", time.Second*10, "Maximum duration before timing out read of the request")
+	flags.DurationP("server.write-timeout", "", time.Second*15, "Maximum duration before timing out write of the response")
+	flags.DurationP("server.idle-timeout", "", time.Second*120, "Maximum duration before timing out idle keep-alive connection")
 	// manifest.json is created as a part of the gulp production "build" task,
 	// it maps fingerprinted asset names to regular asset names, for example:
 	// {"js/main.css": "js/e2a3ff9-main.css"}.
 	// This should only be set to true if doing asset fingerprinting.
-	flags.BoolP("assets-manifest", "", true, "Use manifest.json for mapping asset names to fingerprinted assets")
-
+	flags.BoolP("server.assets-manifest", "", true, "Use manifest.json for mapping asset names to fingerprinted assets")
 	// This should be used in development mode to prevent browser caching of assets
-	flags.BoolP("assets-no-cache", "", false, "Disable browsers caching asset files by setting response headers")
+	flags.BoolP("server.assets-no-cache", "", false, "Disable browsers caching asset files by setting response headers")
 	// This should be used in development mode to avoid having to reload the
 	// server on every template file modification.
-	flags.BoolP("render-recompile", "", false, "Enable recompilation of the template on each render")
+	flags.BoolP("server.render-recompile", "", false, "Enable recompilation of the template on each render")
 	// Defined in app/sessions.go -- Usually cookie storer for dev and disk storer for prod.
-	flags.BoolP("sessions-dev-storer", "", false, "Use the development mode sessions storer (defined in app/sessions.go)")
-	flags.BoolP("enforce-migration", "", true, "Throw error on app start if database is not using latest migration")
-	flags.BoolP("version", "", false, "Display the build version hash")
-	flags.StringP("env", "e", "prod", "The config files environment to load")
+	flags.BoolP("server.sessions-dev-storer", "", false, "Use the development mode sessions storer (defined in app/sessions.go)")
+
+	return flags
+}
+
+func NewDBFlagSet() *pflag.FlagSet {
+	flags := &pflag.FlagSet{}
+
+	// db subsection flags
+	flags.StringP("db.db", "", "", "The database software (postgres|mysql)")
+	flags.StringP("db.dbname", "", "", "The database name to connect to")
+	flags.StringP("db.host", "", "", "The database hostname, e.g localhost")
+	flags.IntP("db.port", "", 0, "The database port")
+	flags.StringP("db.user", "", "", "The database username")
+	flags.StringP("db.pass", "", "", "The database password")
+	flags.StringP("db.sslmode", "", "", "The database sslmode")
+	flags.BoolP("db.enforce-migrations", "", true, "Throw error on app start if database is not using latest migration")
 
 	return flags
 }
@@ -184,6 +282,9 @@ func NewSubViper(flags *pflag.FlagSet, path string) (*viper.Viper, error) {
 	env := v.GetString("env")
 
 	v = v.Sub(env)
+
+	v.BindEnv("server.tls-cert-file", strings.Join([]string{envAppName, "SERVER_TLS_CERT_FILE"}, "_"))
+
 	if v == nil {
 		return nil, fmt.Errorf("unable to load environment %q from %q", env, path)
 	}
@@ -203,7 +304,7 @@ func NewSubViper(flags *pflag.FlagSet, path string) (*viper.Viper, error) {
 func ConfigureViper(v *viper.Viper, path string) error {
 	v.SetConfigType("toml")
 	v.SetConfigFile(path)
-	v.SetEnvPrefix("THINGYMABOB")
+	v.SetEnvPrefix(envAppName)
 	v.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
 	if err := v.ReadInConfig(); err != nil {
 		return err
@@ -213,64 +314,11 @@ func ConfigureViper(v *viper.Viper, path string) error {
 	return nil
 }
 
-// LoadServerConfig loads the config.toml server configuration object
-func LoadServerConfig(cfg interface{}, v *viper.Viper) error {
+// LoadAppConfig loads the config.toml server configuration object
+func LoadAppConfig(cfg interface{}, v *viper.Viper) error {
+	v.Unmarshal(cfg)
+
 	val := reflect.Indirect(reflect.ValueOf(cfg))
-
-	env := v.GetString("env")
-
-	vs := v.Sub("server")
-	vs.Unmarshal(cfg)
-	return nil
-
-	// Find *ServerConfig and set object appropriately
-	for i := 0; i < val.NumField(); i++ {
-		serverCfg, ok := val.Field(i).Interface().(*ServerConfig)
-		if !ok {
-			continue
-		}
-
-		sv := v.Sub("server")
-		// sv.BindPFlags(v.)
-		v.Debug()
-		if sv == nil {
-			return errors.New("unable to find \"server\" subsection in config")
-		}
-		fmt.Printf("BIND BEFORE: %s\n", v.GetString("bind"))
-		fmt.Printf("BIND AFTER: %s\n", sv.GetString("bind"))
-
-		serverCfg.ActiveEnv = env
-		serverCfg.LiveReload = sv.GetBool("live-reload")
-		serverCfg.ProdLogger = sv.GetBool("prod-logger")
-		serverCfg.Bind = sv.GetString("bind")
-		serverCfg.TLSBind = sv.GetString("tls-bind")
-		serverCfg.TLSCertFile = sv.GetString("tls-cert-file")
-		serverCfg.TLSKeyFile = sv.GetString("tls-key-file")
-		serverCfg.ReadTimeout = sv.GetDuration("read-timeout")
-		serverCfg.WriteTimeout = sv.GetDuration("write-timeout")
-		serverCfg.IdleTimeout = sv.GetDuration("idle-timeout")
-		serverCfg.AssetsManifest = sv.GetBool("assets-manifest")
-		serverCfg.AssetsNoCache = sv.GetBool("assets-no-cache")
-		serverCfg.RenderRecompile = sv.GetBool("render-recompile")
-		serverCfg.SessionsDevStorer = sv.GetBool("sessions-dev-storer")
-		serverCfg.PublicPath = sv.GetString("public-path")
-
-		// Finished working on the server cfg struct, so break out
-		break
-	}
-
-	return nil
-}
-
-// LoadDBConfig loads the config.toml db configuration object
-func LoadDBConfig(cfg interface{}, v *viper.Viper) error {
-	val := reflect.Indirect(reflect.ValueOf(cfg))
-
-	env := v.GetString("env")
-
-	vs := v.Sub("server")
-	vs.Unmarshal(cfg)
-	return nil
 
 	// Find *DBConfig and set object appropriately
 	for i := 0; i < val.NumField(); i++ {
@@ -279,21 +327,11 @@ func LoadDBConfig(cfg interface{}, v *viper.Viper) error {
 			continue
 		}
 
-		// if sv is nil it means that there was no [db] section in the toml
+		// if dbCfg is nil it means that there was no [db] section in the toml
 		// file, and so db loading should be skipped
-		sv := v.Sub("db")
-		if sv == nil {
+		if dbCfg == nil {
 			break
 		}
-
-		dbCfg.DB = sv.GetString("db")
-		dbCfg.DBName = sv.GetString("dbname")
-		dbCfg.EnforceMigration = sv.GetBool("enforce-migration")
-		dbCfg.Host = sv.GetString("host")
-		dbCfg.Pass = sv.GetString("pass")
-		dbCfg.Port = sv.GetInt("port")
-		dbCfg.SSLMode = sv.GetString("sslmode")
-		dbCfg.User = sv.GetString("user")
 
 		if dbCfg.DB == "postgres" {
 			if dbCfg.Port == 0 {
--- abcconfig/config_test.go
+++ abcconfig/config_test.go
@@ -2,6 +2,7 @@ package abcconfig
 
 import (
 	"io/ioutil"
+	"os"
 	"testing"
 )
 
@@ -12,7 +13,7 @@ func TestNewFlagSet(t *testing.T) {
 		t.Error("expected non-nil")
 	}
 
-	val, err := flags.GetString("bind")
+	val, err := flags.GetString("server.bind")
 	if err != nil {
 		t.Error(err)
 	}
@@ -48,6 +49,16 @@ func TestInitAppConfig(t *testing.T) {
 
 	// overwrite filename over in config.go
 	filename = file.Name()
+	SetEnvAppName("ABCWEB")
+
+	err = os.Setenv("ABCWEB_SERVER_TLS_CERT_FILE", "bananas")
+	if err != nil {
+		t.Error(err)
+	}
+	err = os.Setenv("ABCWEB_DB_DB", "mydb")
+	if err != nil {
+		t.Error(err)
+	}
 
 	cfg := NewAppConfig()
 	flags := NewFlagSet()
@@ -56,35 +67,128 @@ func TestInitAppConfig(t *testing.T) {
 	}
 
 	// default values should be set appropriately
-	if cfg.Server.ActiveEnv != "prod" {
-		t.Errorf("expected activeenv %q, got %q", "prod", cfg.Server.ActiveEnv)
-	}
 	if cfg.Server.Bind != ":80" {
 		t.Errorf("expected bind %q, got %q", ":80", cfg.Server.Bind)
 	}
 	if cfg.Server.LiveReload != true {
 		t.Error("expected livereload true, got false")
 	}
+	if cfg.Server.TLSBind != "hahaha" {
+		t.Errorf("expected %q, got %q", "hahaha", cfg.Server.TLSBind)
+	}
+	if cfg.Server.TLSCertFile != "bananas" {
+		t.Errorf("expected env var to set tls cert file to %q, got %q", "bananas", cfg.Server.TLSCertFile)
+	}
 
-	//	cfg = NewAppConfig()
-	//
-	//	// test flag override
-	//	if err := flags.Set("env", "cool"); err != nil {
-	//		t.Error(err)
-	//	}
-	//	if err := flags.Set("bind", ":9000"); err != nil {
-	//		t.Error(err)
-	//	}
-	//
-	//	if err := InitAppConfig(flags, cfg); err != nil {
-	//		t.Error(err)
-	//	}
-	//
-	//	// default values should be set appropriately
-	//	if cfg.Server.ActiveEnv != "cool" {
-	//		t.Errorf("expected %q, got %q", "cool", cfg.Server.ActiveEnv)
-	//	}
-	//	if cfg.Server.Bind != ":9000" {
-	//		t.Errorf("expected %q, got %q", ":9000", cfg.Server.Bind)
-	//	}
+	cfg = NewAppConfig()
+	flags = NewFlagSet()
+
+	// test flag override
+	if err := flags.Set("env", "cool"); err != nil {
+		t.Error(err)
+	}
+	if err := flags.Set("server.bind", ":9000"); err != nil {
+		t.Error(err)
+	}
+
+	if err := InitAppConfig(flags, cfg); err != nil {
+		t.Error(err)
+	}
+
+	// default values should be set appropriately
+	if cfg.Server.Bind != ":9000" {
+		t.Errorf("expected %q, got %q", ":9000", cfg.Server.Bind)
+	}
+
+	// test with db section
+	contents = []byte(`[prod]
+	[prod.server]
+		live-reload = true
+		tls-bind = "hahaha"
+[cool]
+	[cool.db]
+		db = "postgres"
+`)
+
+	cfg = NewAppConfig()
+	flags = NewFlagSet()
+
+	// test flag override
+	if err := flags.Set("env", "cool"); err != nil {
+		t.Error(err)
+	}
+
+	if err := InitAppConfig(flags, cfg); err != nil {
+		t.Error(err)
+	}
+}
+
+type Test struct {
+	A string `mapstructure:"a" env:"A"`
+	B TestB  `mapstructure:"b"`
+}
+
+type TestB struct {
+	BA string `mapstructure:"ba" env:"B_BA"`
+	BB string `mapstructure:"bb" env:"B_BB"`
+	C  TestC  `mapstructure:"c"`
+	// unexported field test
+	zz string
+	// unexported pointer test
+	xx *string
+	// exported non-struct pointer test
+	YY *string `mapstructure:"yy" env:"B_YY"`
+
+	// test pointers to structs
+	D *TestD `mapstructure:"d"`
+}
+
+type TestC struct {
+	CA string `mapstructure:"ca" env:"B_C_CA"`
+	CB string `mapstructure:"cb" env:"B_C_CB"`
+}
+
+type TestD struct {
+	DA string `mapstructure:"da" env:"B_D_DA"`
+}
+
+func TestGetTagMappings(t *testing.T) {
+	i := ""
+	j := ""
+	cfg := &Test{
+		B: TestB{
+			xx: &i,
+			YY: &j,
+			C:  TestC{},
+			D:  &TestD{},
+		},
+	}
+
+	mappings, err := GetTagMappings(cfg)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := []mapping{
+		{chain: "a", env: "A"},
+		{chain: "b.ba", env: "B_BA"},
+		{chain: "b.bb", env: "B_BB"},
+		{chain: "b.c.ca", env: "B_C_CA"},
+		{chain: "b.c.cb", env: "B_C_CB"},
+		{chain: "b.yy", env: "B_YY"},
+		{chain: "b.d.da", env: "B_D_DA"},
+	}
+
+	if len(mappings) != len(expected) {
+		t.Errorf("expected len %d, got %d", len(expected), len(mappings))
+	}
+
+	for i, m := range mappings {
+		if expected[i].chain != m.chain {
+			t.Errorf("expected chain: %s, got: %s", expected[i].chain, m.chain)
+		}
+		if expected[i].env != m.env {
+			t.Errorf("expected env: %s, got: %s", expected[i].env, m.env)
+		}
+	}
 }
