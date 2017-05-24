463c118d4befc5ce02b17ba49c046c09d6f14290
Patrick O'brien <nullbio@users.noreply.github.com>
2017-05-25 01:34:20 +1000

    add errors middleware

---
+++ abcmiddleware/errors.go
@@ -0,0 +1,154 @@
+package abcmiddleware
+
+import (
+	"errors"
+	"net/http"
+	"reflect"
+
+	chimiddleware "github.com/pressly/chi/middleware"
+	"github.com/volatiletech/abcmiddleware"
+	"github.com/volatiletech/abcrender"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// These are the errors that can be used in your controllers and matched
+// against in the Errors middleware to perform special actions.
+var (
+	ErrUnauthorized = errors.New("not authorized")
+	ErrForbidden    = errors.New("access is forbidden")
+)
+
+type ErrorContainer struct {
+	// The error that will be returned by the controller
+	Err error
+	// The template file top render (e.g "errors/500")
+	Template string
+	// HTTP status code to respond with
+	Code int
+	// A custom handler to perform additional operations on this error
+	Handler ErrorHandler
+}
+
+type errorManager struct {
+	render abcrender.Renderer
+	errors []ErrorContainer
+}
+
+// NewErrorManager creates an error manager that can be used to
+// create an error handler to wrap your controllers with
+func NewErrorManager(render abcrender.Renderer) *errorManager {
+	return &errorManager{
+		render: render,
+		errors: []ErrorContainer{},
+	}
+}
+
+// Create a new ErrorContainer that can be added to an errorManager.
+// If you provide a handler here (instead of nil) then the Errors middleware
+// will use your handler opposed to taking the default route of logging
+// and rendering. You must handle logging and rendering yourself.
+func NewError(err error, code int, template string, handler ErrorHandler) ErrorContainer {
+	if err == nil {
+		panic("cannot supply nil error")
+	}
+
+	// template and code must be set if handler is nil
+	if handler == nil && (len(template) == 0 || code == 0) {
+		panic("template and code must be set if handler is nil")
+	}
+
+	return ErrorContainer{
+		Err:      err,
+		Code:     code,
+		Template: template,
+		Handler:  handler,
+	}
+}
+
+// Remove a ErrorContainer from the error manager
+func (m *errorManager) Remove(e ErrorContainer) {
+	for i, v := range m.errors {
+		if reflect.DeepEqual(v, e) {
+			m.errors = append(m.errors[:i], m.errors[i+1:]...)
+		}
+	}
+}
+
+// Add a new ErrorContainer to the error manager
+func (m *errorManager) Add(e ErrorContainer) {
+	m.errors = append(m.errors, e)
+}
+
+// AppHandler is the function signature for controllers that return errors.
+type AppHandler func(w http.ResponseWriter, r *http.Request) error
+
+// ErrorHandler is the function signature for user supplied error handlers.
+type ErrorHandler func(w http.ResponseWriter, r *http.Request, e ErrorContainer, render abcrender.Renderer) error
+
+// Errors is a middleware to handle controller errors and error page rendering.
+// The benefit of using this middleware opposed to logging and rendering
+// errors directly in your controller is that it's all centralized to one
+// location which simplifies adding notifiers (like slack and email).
+// It also reduces a lot of controller boilerplate.
+func (m *errorManager) Errors(ctrl AppHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := ctrl(w, r)
+		if err == nil {
+			return
+		}
+
+		var container ErrorContainer
+		var template string
+		var code int
+
+		found := false
+		for _, e := range m.errors {
+			if e.Err == err {
+				container = e
+				found = true
+				break
+			}
+		}
+
+		if !found { // no error containers/handlers found, default path
+			code = http.StatusInternalServerError
+			template = "errors/500"
+		} else if container.Handler != nil { // container and handler are set
+			err := container.Handler(w, r, container, m.render)
+			if err != nil {
+				panic(err)
+			}
+			// Users handlers should handle EVERYTHING, so return here
+			// once the handler has been called successfully above.
+			return
+		} else { // container is set and handler is nil
+			code = container.Code
+			template = container.Template
+		}
+
+		// Get the Request ID scoped logger
+		log := abcmiddleware.Log(r)
+
+		fields := []zapcore.Field{
+			zap.String("method", r.Method),
+			zap.String("uri", r.RequestURI),
+			zap.Bool("tls", r.TLS != nil),
+			zap.String("protocol", r.Proto),
+			zap.String("host", r.Host),
+			zap.String("remote_addr", r.RemoteAddr),
+			zap.Error(err),
+		}
+
+		// log with the request_id scoped logger
+		switch code {
+		case http.StatusInternalServerError:
+			log.Error("request error", fields...)
+			requestID := chimiddleware.GetReqID(r.Context())
+			m.render.HTML(w, code, template, requestID)
+		default: // warn does not log stacktrace in prod, but error and above does
+			log.Warn("request failed", fields...)
+			m.render.HTML(w, code, template, nil)
+		}
+	}
+}
---
+++ abcmiddleware/errors_test.go
@@ -0,0 +1,147 @@
+package abcmiddleware
+
+import (
+	"context"
+	"errors"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+
+	"go.uber.org/zap"
+
+	"github.com/volatiletech/abcrender"
+)
+
+func TestRemoveAndAdd(t *testing.T) {
+	t.Parallel()
+
+	ea := errors.New("error1")
+	eb := errors.New("error2")
+
+	eaExpected := ErrorContainer{
+		Err:      ea,
+		Template: "errors/404",
+		Code:     404,
+		Handler:  nil,
+	}
+
+	ebExpected := ErrorContainer{
+		Err:      eb,
+		Template: "errors/404",
+		Code:     404,
+		Handler:  nil,
+	}
+
+	m := NewErrorManager(&abcrender.Render{})
+
+	m.Add(NewError(ea, 404, "errors/404", nil))
+	m.Add(NewError(eb, 404, "errors/404", nil))
+
+	if len(m.errors) != 2 {
+		t.Errorf("expected len 2, got %d", len(m.errors))
+	}
+	if !reflect.DeepEqual(m.errors[0], eaExpected) {
+		t.Errorf("expected:\n%#v\ngot:\n%#v", eaExpected, m.errors[0])
+	}
+	if !reflect.DeepEqual(m.errors[1], ebExpected) {
+		t.Errorf("expected:\n%#v\ngot:\n%#v", ebExpected, m.errors[1])
+	}
+
+	m.Remove(m.errors[0])
+
+	if len(m.errors) != 1 {
+		t.Error("expected len 1, got %d", len(m.errors))
+	}
+
+	if !reflect.DeepEqual(m.errors[0], ebExpected) {
+		t.Errorf("expected:\n%#v\ngot:\n%#v", ebExpected, m.errors[1])
+	}
+
+	m.Remove(m.errors[0])
+	if len(m.errors) != 0 {
+		t.Error("expected len 0, got %d", len(m.errors))
+	}
+}
+
+func TestCustomErrorHandler(t *testing.T) {
+	t.Parallel()
+
+	var globalTest bool
+	myHandler := func(w http.ResponseWriter, r *http.Request, e ErrorContainer, render abcrender.Renderer) error {
+		globalTest = true
+		return nil
+	}
+
+	// test handler route
+	ea := errors.New("error1")
+	m := NewErrorManager(&abcrender.Render{})
+	m.Add(NewError(ea, 404, "errors/404", myHandler))
+	fn := m.Errors(func(w http.ResponseWriter, r *http.Request) error {
+		return ea
+	})
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("get", "/", nil)
+	fn.ServeHTTP(w, r)
+	if !globalTest {
+		t.Error("expected handler to set globalTest")
+	}
+
+	// test non-handler non-custom error route
+	rndr := &mockRender{}
+	m = NewErrorManager(rndr)
+	fn = m.Errors(func(w http.ResponseWriter, r *http.Request) error {
+		// generic error that isnt added to error manager
+		// this should test default case
+		return errors.New("test")
+	})
+	w = httptest.NewRecorder()
+	r = httptest.NewRequest("get", "/", nil)
+	r = r.WithContext(context.WithValue(context.Background(), CtxLoggerKey, zap.NewNop()))
+	fn.ServeHTTP(w, r)
+	if rndr.status != http.StatusInternalServerError {
+		t.Errorf("expected StatusInternalServerError, got %d", rndr.status)
+	}
+	if rndr.name != "errors/500" {
+		t.Errorf("expected %q, got %q", "errors/500", rndr.name)
+	}
+
+	// test non-handler but custom error route
+	e1 := errors.New("100 error")
+	rndr = &mockRender{}
+	m = NewErrorManager(rndr)
+	m.Add(NewError(e1, 100, "errors/100", nil))
+	fn = m.Errors(func(w http.ResponseWriter, r *http.Request) error {
+		// generic error that isnt added to error manager
+		// this should test default case
+		return e1
+	})
+	w = httptest.NewRecorder()
+	r = httptest.NewRequest("get", "/", nil)
+	r = r.WithContext(context.WithValue(context.Background(), CtxLoggerKey, zap.NewNop()))
+	fn.ServeHTTP(w, r)
+	if rndr.status != 100 {
+		t.Errorf("expected 100, got %d", rndr.status)
+	}
+	if rndr.name != "errors/100" {
+		t.Errorf("expected %q, got %q", "errors/100", rndr.name)
+	}
+}
+
+type mockRender struct {
+	status int
+	name   string
+}
+
+func (mockRender) Data(w io.Writer, status int, v []byte) error      { return nil }
+func (mockRender) JSON(w io.Writer, status int, v interface{}) error { return nil }
+func (mockRender) Text(w io.Writer, status int, v string) error      { return nil }
+func (m *mockRender) HTML(w io.Writer, status int, name string, binding interface{}) error {
+	m.status = status
+	m.name = name
+	return nil
+}
+func (mockRender) HTMLWithLayout(w io.Writer, status int, name string, binding interface{}, layout string) error {
+	return nil
+}
