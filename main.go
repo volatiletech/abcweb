ea494b8dacd1dfc0a91f8a786be7656a4966bfc0
Patrick O'brien <nullbio@users.noreply.github.com>
2017-06-13 17:40:06 +1000

    fix null package in gopkg.toml

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"github.com/volatiletech/abcweb/cmd"
 )
 
-const abcwebVersion = "2.1.0"
+const abcwebVersion = "2.1.1"
 
 func main() {
 	// Too much happens between here and cobra's argument handling, for
