01abb5da67349f1481745b12961d74d9ae5e6668
Patrick O'brien <nullbio@users.noreply.github.com>
2017-06-29 23:47:01 +1000

    Bump version to 2.1.6 (deps cmd fix)

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"github.com/volatiletech/abcweb/cmd"
 )
 
-const abcwebVersion = "2.1.5"
+const abcwebVersion = "2.1.6"
 
 func main() {
 	// Too much happens between here and cobra's argument handling, for
