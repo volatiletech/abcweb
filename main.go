6a0c481cd0b7b270f14e1d4a076eb498885b3aaa
Patrick O'brien <nullbio@users.noreply.github.com>
2017-07-26 15:19:23 +1000

    fix abcweb version

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"github.com/volatiletech/abcweb/cmd"
 )
 
-const abcwebVersion = "3.1.0"
+const abcwebVersion = "3.0.2"
 
 func main() {
 	// Too much happens between here and cobra's argument handling, for
